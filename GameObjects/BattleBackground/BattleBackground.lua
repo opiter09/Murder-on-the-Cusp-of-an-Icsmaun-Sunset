8191839bec7813a6684c3fca42065c0c08e5e854
opiter09 <92452568+opiter09@users.noreply.github.com>
2021-10-29 00:02:55 -0400

    wee bit of dehardcoding

--- GameObjects/BattleBackground/BattleBackground.lua
+++ GameObjects/BattleBackground/BattleBackground.lua
@@ -2,7 +2,7 @@ function Local.Init()
     local vars = vili.from_file("root://saveData.vili")
     local backgrounds = vili.from_file("root://Data/Groups/battleBackgrounds.vili")
 
-    for i = 1, 19 do
+    for i = 1, backgrounds.total do
         if (backgrounds[("Background%s"):format(i)][vars.currentMap] ~= nil) then
             This.Sprite:loadTexture(("sprites://LevelSprites/Backdrops/%s.BMP"):format(backgrounds[("Background%s"):format(i)]["name"]))
             break
